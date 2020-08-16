67e7547f6959d60abcb704c2aed662c6b459adee
Clay Dowling <clay@lazarusid.com>
2020-08-16 17:17:47 -0400

    Working character sheet

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-	"fmt"
-
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"text/template"
 	"github.com/claydowling/genesyscharacter/genesys"
 )
 
-func main() {
-	archetypes := genesys.ReadArchetypeFile("base.arc")
+func getPath(filename string) string {
+	base, _ := os.Executable()
+	startdir := filepath.Join(filepath.Dir(base), "data")
+	return filepath.Join(startdir, filename)
+}
 
-	for idx, a := range archetypes {
-		fmt.Printf("%2d) %s\n", idx, a.Name)
+func main() {
+	archetypes, err := genesys.ReadArchetypeFile(getPath("base/archetypes.yaml"))
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	skills, err := genesys.ReadSkillFile(getPath("base/skills.yaml"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	character, err := genesys.ReadCharacterFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	sheet, err := ioutil.ReadFile(getPath("base/sheet.txt"))
+
+	cc, err := genesys.Calculate(character, archetypes, skills)
+
+	s := template.Must(template.New("sheet").Parse(string(sheet)))
+	s.Execute(os.Stdout, cc)
 }
